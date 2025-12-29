235d2d5712271f3e0d3b268fd3e3c0ee5e3be784
cchahid <c.chahidachraf@gmail.com>
2025-12-29 22:24:49 +0100

    all

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -1,88 +1,143 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN 
-    PRINT '================================================';
-    PRINT 'Loading Bronze Layer';
-    PRINT '================================================';   
-    -- Test if SQL Server can see the file
-    EXEC xp_fileexist '/data/source_crm/cust_info.csv';
-    EXEC xp_fileexist '/data/source_crm/prd_info.csv';
-    EXEC xp_fileexist '/data/source_crm/sales_details.csv';
+    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
+        PRINT '================================================';
+        PRINT 'Loading Bronze Layer';
+        PRINT '================================================';   
+        -- Test if SQL Server can see the file
+        EXEC xp_fileexist '/data/source_crm/cust_info.csv';
+        EXEC xp_fileexist '/data/source_crm/prd_info.csv';
+        EXEC xp_fileexist '/data/source_crm/sales_details.csv';
 
-    PRINT '------------------------------------------------';
-    PRINT 'Loading CRM Tables';
-    PRINT '------------------------------------------------'; 
-    TRUNCATE TABLE bronze.crm_cust_info;
-    BULK INSERT bronze.crm_cust_info
-    FROM '/data/source_crm/cust_info.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        ROWTERMINATOR = '0x0A',
-        TABLOCK
-    );
-    SELECT COUNT(*) AS bronze_crm_cust_info_count FROM bronze.crm_cust_info;
+        PRINT '------------------------------------------------';
+        PRINT 'Loading CRM Tables';
+        PRINT '------------------------------------------------'; 
 
-    TRUNCATE TABLE bronze.crm_prd_info;
-    BULK INSERT bronze.crm_prd_info
-    FROM '/data/source_crm/prd_info.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        ROWTERMINATOR = '0x0A',
-        TABLOCK
-    );
-    SELECT COUNT(*) AS bronze_crm_prd_info_count FROM bronze.crm_prd_info;
+        SET @start_time = GETDATE();
+        PRINT '>> truncating table: bronze.crm_cust_info';
+        TRUNCATE TABLE bronze.crm_cust_info;
 
+        PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+        BULK INSERT bronze.crm_cust_info
+        FROM '/data/source_crm/cust_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '0x0A',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        -- SELECT COUNT(*) AS bronze_crm_cust_info_count FROM bronze.crm_cust_info;
 
-    TRUNCATE TABLE bronze.crm_sales_details;
-    BULK INSERT bronze.crm_sales_details
-    FROM '/data/source_crm/sales_details.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        ROWTERMINATOR = '0x0A',
-        TABLOCK
-    );
-    SELECT COUNT(*) AS bronze_crm_sales_details_count FROM bronze.crm_sales_details;
+        SET @start_time = GETDATE();
+        PRINT '>> truncating table: bronze.crm_prd_info';
+        TRUNCATE TABLE bronze.crm_prd_info;
 
-    -- Test if SQL Server can see the file
-    EXEC xp_fileexist '/data/source_erp/CUST_AZ12.csv';
-    EXEC xp_fileexist '/data/source_erp/LOC_A101.csv';
-    EXEC xp_fileexist '/data/source_erp/PX_CAT_G1V2.csv';
+        PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+        BULK INSERT bronze.crm_prd_info
+        FROM '/data/source_crm/prd_info.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '0x0A',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        -- SELECT COUNT(*) AS bronze_crm_prd_info_count FROM bronze.crm_prd_info;
 
-    PRINT '------------------------------------------------';
-    PRINT 'Loading ERP Tables';
-    PRINT '------------------------------------------------'; 
-    TRUNCATE TABLE bronze.erp_cust_az12;
-    BULK INSERT bronze.erp_cust_az12
-    FROM '/data/source_erp/CUST_AZ12.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        ROWTERMINATOR = '0x0A',
-        TABLOCK
-    );
-    SELECT COUNT(*) AS bronze_erp_cust_az12_count FROM bronze.erp_cust_az12;
+        SET @start_time = GETDATE();
+        PRINT '>> truncating table: bronze.crm_sales_details';
+        TRUNCATE TABLE bronze.crm_sales_details;
 
-    TRUNCATE TABLE bronze.erp_loc_a101;
-    BULK INSERT bronze.erp_loc_a101
-    FROM '/data/source_erp/LOC_A101.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        ROWTERMINATOR = '0x0A',
-        TABLOCK
-    );
-    SELECT COUNT(*) AS  bronze_erp_loc_a101_count FROM  bronze.erp_loc_a101;
+        PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+        BULK INSERT bronze.crm_sales_details
+        FROM '/data/source_crm/sales_details.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '0x0A',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        -- SELECT COUNT(*) AS bronze_crm_sales_details_count FROM bronze.crm_sales_details;
 
-    TRUNCATE TABLE bronze.erp_px_cat_g1v2
-    BULK INSERT bronze.erp_px_cat_g1v2
-    FROM '/data/source_erp/PX_CAT_G1V2.csv'
-    WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        ROWTERMINATOR = '0x0A',
-        TABLOCK
-    );
-    SELECT COUNT(*) AS bronze_erp_px_cat_g1v2_count FROM bronze.erp_px_cat_g1v2;
+        -- Test if SQL Server can see the file
+        EXEC xp_fileexist '/data/source_erp/CUST_AZ12.csv';
+        EXEC xp_fileexist '/data/source_erp/LOC_A101.csv';
+        EXEC xp_fileexist '/data/source_erp/PX_CAT_G1V2.csv';
+
+        PRINT '------------------------------------------------';
+        PRINT 'Loading ERP Tables';
+        PRINT '------------------------------------------------'; 
+
+        SET @start_time = GETDATE();
+        PRINT '>> truncating table: bronze.erp_cust_az12';
+        TRUNCATE TABLE bronze.erp_cust_az12;
+
+        PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+        BULK INSERT bronze.erp_cust_az12
+        FROM '/data/source_erp/CUST_AZ12.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '0x0A',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        -- SELECT COUNT(*) AS bronze_erp_cust_az12_count FROM bronze.erp_cust_az12;
+
+        SET @start_time = GETDATE();
+        PRINT '>> truncating table: bronze.erp_loc_a101';
+        TRUNCATE TABLE bronze.erp_loc_a101;
+
+        PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+        BULK INSERT bronze.erp_loc_a101
+        FROM '/data/source_erp/LOC_A101.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '0x0A',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        -- SELECT COUNT(*) AS  bronze_erp_loc_a101_count FROM  bronze.erp_loc_a101;
+
+        SET @start_time = GETDATE();
+        PRINT '>> truncating table: bronze.erp_px_cat_g1v2';
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+        PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+        BULK INSERT bronze.erp_px_cat_g1v2
+        FROM '/data/source_erp/PX_CAT_G1V2.csv'
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            ROWTERMINATOR = '0x0A',
+            TABLOCK
+        );
+        SET @end_time = GETDATE();
+        PRINT '>> Load Duration : ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+        -- SELECT COUNT(*) AS bronze_erp_px_cat_g1v2_count FROM bronze.erp_px_cat_g1v2;
+
+        SET @batch_end_time = GETDATE();
+        PRINT '================================================';
+        PRINT 'Loading Bronze Layer is Completed';
+        PRINT '  - Total Load Duration : ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+        PRINT '================================================'; 
+    END TRY
+    BEGIN CATCH
+        PRINT '================================================';
+        PRINT 'ERROR OCCURE DURING LOADING BRONZE LAYER';
+        PRINT 'Error Message' + ERROR_MESSAGE();
+        PRINT 'Error Number' + CAST(ERROR_NUMBER() AS NVARCHAR);
+        PRINT 'Error Number' + CAST(ERROR_STATE() AS NVARCHAR);
+        PRINT '================================================';
+    END CATCH
 END
